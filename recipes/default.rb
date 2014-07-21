ba5cdc00f9ea7023e386abf986a8cfcd29fdd57e
daniel-nelson <daniel.wolcott.nelson@gmail.com>
2014-07-21 17:56:06 -0500

    BUNDLE_PATH, not GEM_PATH

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,5 +10,5 @@
 cron 'cronjobs.rake' do
   user 'deploy'
   minute '*/5'
-  command "cd /srv/www/touchstream/current && NEWRELIC_DISPATCHER='rake' GEM_PATH='/home/deploy/.bundler/touchstream/ruby/2.1.0/gems' /usr/local/bin/bundle exec rake cron"
+  command "cd /srv/www/touchstream/current && NEWRELIC_DISPATCHER='rake' BUNDLE_PATH='/home/deploy/.bundler/touchstream/ruby/2.1.0/gems' /usr/local/bin/bundle exec rake cron"
 end
