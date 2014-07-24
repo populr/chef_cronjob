f58bf680eab8af0c05c7997770c3b4d6f0ae21fd
Daniel Nelson <daniel@populr.me>
2014-07-24 16:19:26 -0500

    Execute cronjobs as deploy (setting the cronjob on deploy user failed in OpsWorks Rails stack)

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,6 +10,6 @@
 node[:deploy].each do |application, deploy|
   cron 'cronjobs_rake' do
     minute '*/5'
-    command "cd #{deploy[:deploy_to]}/current && RAILS_ENV=#{deploy[:rails_env]} NEWRELIC_DISPATCHER='rake' PATH='/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/aws/bin:/home/ec2-user/bin:/opt/aws/bin:/opt/aws/bin' bundle exec rake cron"
+    command "sudo su deploy && cd #{deploy[:deploy_to]}/current && RAILS_ENV=#{deploy[:rails_env]} NEWRELIC_DISPATCHER='rake' PATH='/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/aws/bin:/home/ec2-user/bin:/opt/aws/bin:/opt/aws/bin' bundle exec rake cron"
   end
 end
