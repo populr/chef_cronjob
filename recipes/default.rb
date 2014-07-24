63feb57937dcd91bfb525e674d89f347480dde59
daniel-nelson <daniel.wolcott.nelson@gmail.com>
2014-07-23 22:36:49 -0500

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,5 +9,5 @@
 
 cron 'cronjobs_rake' do
   minute '*/5'
-  command 'cd /srv/www/touchstream/current && NEWRELIC_DISPATCHER="rake" PATH="/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/aws/bin:/home/ec2-user/bin:/opt/aws/bin:/opt/aws/bin" bundle exec rake cron'
+  command 'cd /srv/www/touchstream/current && RAILS_ENV=production NEWRELIC_DISPATCHER="rake" PATH="/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/aws/bin:/home/ec2-user/bin:/opt/aws/bin:/opt/aws/bin" bundle exec rake cron'
 end
