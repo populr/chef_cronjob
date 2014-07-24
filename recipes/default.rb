51c59500265a278cba59657e1ad4c33d6516ef24
Daniel Nelson <daniel@populr.me>
2014-07-24 10:33:05 -0500

    Use node variables to configure cronjob

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,9 @@
 # All rights reserved - Do Not Redistribute
 #
 
-cron 'cronjobs_rake' do
-  minute '*/5'
-  command 'cd /srv/www/touchstream/current && RAILS_ENV=production NEWRELIC_DISPATCHER="rake" PATH="/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/aws/bin:/home/ec2-user/bin:/opt/aws/bin:/opt/aws/bin" bundle exec rake cron'
+node[:deploy].each do |application, deploy|
+  cron 'cronjobs_rake' do
+    minute '*/5'
+    command "cd #{deploy[:deploy_to]}/current && RAILS_ENV=#{deploy[:rails_env]} NEWRELIC_DISPATCHER='rake' PATH='/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/aws/bin:/home/ec2-user/bin:/opt/aws/bin:/opt/aws/bin' bundle exec rake cron"
+  end
 end
